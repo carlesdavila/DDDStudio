9c320b78586076d7f703511ee00b6ea2bf2d8f9f
Carles <carles.davila@gmail.com>
2024-05-26 19:23:22 +0200

    Added Cocona and first command

--- src/ddd/Program.cs
+++ src/ddd/Program.cs
@@ -1,16 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
+using Cocona;
+using DDDCanvasCreator;
 
-using System.Reflection;
+var app = CoconaApp.Create();
 
-ShowLogo("starting...!");
-ShowVersion();
+
+app.AddCommand("GenerateSvgs", ([Argument] string inputDirectory, string outputDirectory) =>
+    {
+        var dddCanvas = new DDDCanvas();
+
+        if (!Directory.Exists(inputDirectory))
+        {
+            Console.WriteLine($"The input directory '{inputDirectory}' does not exist.");
+            return;
+        }
+
+        if (!Directory.Exists(outputDirectory)) Directory.CreateDirectory(outputDirectory);
+
+        var yamlFiles = Directory.GetFiles(inputDirectory, "*.yaml");
+        foreach (var yamlFile in yamlFiles)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(yamlFile);
+            var outputFilePath = Path.Combine(outputDirectory, $"{fileName}.svg");
+
+            try
+            {
+                dddCanvas.GenerateSvg(yamlFile, outputFilePath);
+                Console.WriteLine($"Generated SVG for {yamlFile} -> {outputFilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to generate SVG for {yamlFile}: {ex.Message}");
+            }
+        }
+    })
+    .WithDescription("Generate all svgs from yaml files");
+
+
+app.AddCommand("version", () =>
+    {
+        ShowLogo("starting...!");
+        ShowVersion();
+    })
+    .WithDescription("Client Version");
+app.Run();
+
+
 return;
 
 
 static void ShowLogo(string message)
 {
- string logo = $"\n        {message}";
- logo += @"
+    var logo = $"\n        {message}";
+    logo += @"
 
   _____  _____  _____     _____ _             _ _        
  |  __ \|  __ \|  __ \   / ____| |           | (_)       
@@ -20,18 +62,18 @@ static void ShowLogo(string message)
  |_____/|_____/|_____/ _|_____/ \__|\__,_|\__,_|_|\___/  
                
 ";
- 
- Console.WriteLine(logo);
+
+    Console.WriteLine(logo);
 }
 
 static void ShowVersion()
 {
- var assembly = Assembly.GetExecutingAssembly();
+    var assembly = Assembly.GetExecutingAssembly();
 
- var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
-               ?? assembly.GetName().Version?.ToString()
-               ?? "Version not found";
- var cleanVersion = version.Split('+')[0];
+    var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                  ?? assembly.GetName().Version?.ToString()
+                  ?? "Version not found";
+    var cleanVersion = version.Split('+')[0];
 
- Console.WriteLine($"Version: {cleanVersion}");
+    Console.WriteLine($"Version: {cleanVersion}");
 }
