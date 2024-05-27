10f7e3f101da3beae2d94663be4940d307ca7987
Carles <carles.davila@gmail.com>
2024-05-27 23:13:11 +0200

    add build

--- tests/ddd.Tests/Commands/AddSubdomainCommandTests.cs
+++ tests/ddd.Tests/Commands/AddSubdomainCommandTests.cs
@@ -1,6 +1,21 @@
-﻿namespace ddd.Tests.Commands;
+﻿using Cocona;
+using ddd.Commands;
+
+namespace ddd.Tests.Commands;
 
 public class AddSubdomainCommandTests
 {
-    
+    [Fact(Skip = "review")]
+    public void AddSubdomain_CommandExecutes_Successfully()
+    {
+        // Arrange
+        var args = new[] { "add-subdomain", "TestSubdomain" };
+        CoconaApp.Run<InitCommand>();
+
+        // Act
+        CoconaApp.Run<AddSubdomainCommand>(args);
+
+        // Assert
+        Assert.True(Directory.Exists(Path.Combine("DDD", "Subdomains", "TestSubdomain")));
+    }
 }
