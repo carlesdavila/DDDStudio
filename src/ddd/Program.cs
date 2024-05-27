0c52469bc7857e8b564d40b234c4919bbd575462
Carles <carles.davila@gmail.com>
2024-05-27 23:51:00 +0200

    delete old generate svgs

--- src/ddd/Program.cs
+++ src/ddd/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Cocona;
 using ddd.Commands;
-using DDDCanvasCreator;
 
 var app = CoconaApp.Create();
 
@@ -15,38 +14,5 @@ app.AddCommands<AddContextCommand>();
 app.AddCommands<AddAggregateCommand>();
 
 app.AddCommands<GenerateDocsCommand>();
-
-
-
-app.AddCommand("GenerateSvgs", ([Argument] string inputDirectory, string outputDirectory) =>
-    {
-        var dddCanvas = new DDDCanvas();
-
-        if (!Directory.Exists(inputDirectory))
-        {
-            Console.WriteLine($"The input directory '{inputDirectory}' does not exist.");
-            return;
-        }
-
-        if (!Directory.Exists(outputDirectory)) Directory.CreateDirectory(outputDirectory);
-
-        var yamlFiles = Directory.GetFiles(inputDirectory, "*.yaml");
-        foreach (var yamlFile in yamlFiles)
-        {
-            var fileName = Path.GetFileNameWithoutExtension(yamlFile);
-            var outputFilePath = Path.Combine(outputDirectory, $"{fileName}.svg");
-
-            try
-            {
-                dddCanvas.GenerateSvg(yamlFile, outputFilePath);
-                Console.WriteLine($"Generated SVG for {yamlFile} -> {outputFilePath}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to generate SVG for {yamlFile}: {ex.Message}");
-            }
-        }
-    })
-    .WithDescription("Generate all svgs from yaml files");
 
 app.Run();
